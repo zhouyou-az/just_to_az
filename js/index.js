1cf1ac7d080ec3c770668b8486c573fc25e6f889
386258412@qq.com <386258412@qq.com>
2020-09-25 17:03:37 +0800

    s

--- js/index.js
+++ js/index.js
@@ -11,8 +11,8 @@ function openLetter() {
 
     tl.to(".topfold", 0.2, { transformOrigin: "50% 0%", x: 0, y: 0, fill: '#d5d3bb', scaleY: -1, delay: 0, ease: Circ.easeIn });
     tl.to(".letter", 0.2, { x: 0, y: -140, delay: 0, height: 125, ease: Circ.easeOut });
-    tl.to(".upper", 1, { x: 0, transformOrigin: "50% 100%", rotationX: -180, delay: 0, height: 125, ease: Circ.easeOut });
-    tl.to(".letter", 0.2, { x: 0, y: -200, rotationX: 0,scaleY: 1, delay: 0, ease: Circ.easeInOut });
+    tl.to(".upper", 0.5, { x: 0, transformOrigin: "50% 100%", rotationX: -180, delay: 0, height: 125, ease: Circ.easeOut });
+    tl.to(".letter", 0.1, { x: 0, y: -200, rotationX: 0,scaleY: 1, delay: 0, ease: Circ.easeInOut });
     flag = false;
 
   } else if(!flag) {
@@ -21,9 +21,9 @@ function openLetter() {
       delay: 0
     });
 
-    tl2.to(".upper", 0.5, { x: 0, transformOrigin: "50% 100%", rotationX: 0, delay: 0, height: 125, ease: Circ.easeIn });
-    tl2.to(".letter", 0.5, { x: 0, y: 0, rotationX: 0, scaleY: 0.5, delay: 0, ease: Expo.easeOut });
-    tl2.to(".topfold", 0.3, { transformOrigin: "50% 0%", scaleY: 1, delay: 0, ease: Circ.easeIn })
+    tl2.to(".upper", 0.3, { x: 0, transformOrigin: "50% 100%", rotationX: 0, delay: 0, height: 125, ease: Circ.easeIn });
+    tl2.to(".letter", 2, { x: 0, y: 0, rotationX: 0, scaleY: 0.5, delay: 0, ease: Expo.easeOut });
+    tl2.to(".topfold", 2, { transformOrigin: "50% 0%", scaleY: 1, delay: 0, ease: Circ.easeIn })
     flag = true;
   }
 
