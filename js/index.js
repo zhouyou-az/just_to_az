c8a5b2c054934599daa16b65e612320f7f7332e0
386258412@qq.com <386258412@qq.com>
2020-09-25 17:43:07 +0800

    z

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 
 
 let flag = true
+let startTimer;
+let endTimer
 function openLetter() {
+  // clearTimeout(endTimeTimer);
   if (flag) {
     var tl = new TimelineMax({
       repeat: 0,
@@ -12,10 +15,22 @@ function openLetter() {
     tl.to(".topfold", 0.5, { transformOrigin: "50% 0%", x: 0, y: 0, fill: '#d5d3bb', scaleY: -1, delay: 0, ease: Circ.easeIn });
     tl.to(".letter", 0.3, { x: 0, y: -140, delay: 0, height: 125, ease: Circ.easeOut });
     tl.to(".upper", 0.5, { x: 0, transformOrigin: "50% 100%", rotationX: -180, delay: 0, height: 125, ease: Circ.easeOut });
-    tl.to(".letter", 0.2, { x: 0, y: -200, rotationX: 0,scaleY: 1, delay: 0, ease: Circ.easeInOut });
+    tl.to(".letter", 0.2, { x: 0, y: -200, rotationX: 0, scaleY: 1, delay: 0, ease: Circ.easeInOut });
+
+
+    startTimer=  $(".my-text").each(function (index) {
+      var _this = this
+      var sss = flag 
+      setTimeout(function () {
+        if(sss)
+        $(_this).animate({ "opacity": 1 }, 1000).siblings();
+      }, 1500 * index + 2000);
+    })
+
     flag = false;
 
-  } else if(!flag) {
+  } else if (!flag) {
+    clearTimeout(startTimer);
     var tl2 = new TimelineMax({
       repeat: 0,
       delay: 0
@@ -25,6 +40,15 @@ function openLetter() {
     tl2.to(".letter", 1, { x: 0, y: 0, rotationX: 0, scaleY: 0.5, delay: 0, ease: Expo.easeOut });
     tl2.to(".topfold", 0.6, { transformOrigin: "50% 0%", scaleY: 1, delay: 0, ease: Circ.easeIn });
     flag = true;
+  
+
+    endTimer =  setTimeout(function () {
+      $(".my-text").each(function (index) {
+        var _this = this
+        $(_this).animate({ "opacity": 0 }, 0).siblings();
+      })
+    }, 1000)
+
   }
 
 
