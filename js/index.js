d1094bc46d845985f1b5d4871bb5f832e44d4946
zhouyou <386258412@qq.com>
2020-09-25 13:53:15 +0800

    z

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+
+let flag = 1
+function openLetter() {
+  if (flag==1) {
+    var tl = new TimelineMax({
+      repeat: 0,
+      delay: 0
+    });
+
+
+    tl.to(".topfold", 0.5, { transformOrigin: "50% 0%", x: 0, y: 0, fill: '#d5d3bb', scaleY: -1, delay: 0, ease: Circ.easeIn });
+    tl.to(".letter", 0.5, { x: 0, y: -140, delay: 0, height: 125, ease: Circ.easeOut });
+    tl.to(".upper", 1.2, { x: 0, transformOrigin: "50% 100%", rotationX: -180, delay: 0.3, height: 125, ease: Circ.easeOut });
+    tl.to(".letter", 0.5, { x: 0, y: -200, rotationX: 0, delay: 0, ease: Circ.easeInOut });
+    flag = 2
+
+  } else if(flag==2) {
+    var tl2 = new TimelineMax({
+      repeat: 0,
+      delay: 0
+    });
+
+    tl2.to(".upper", 1, { x: 0, transformOrigin: "50% 100%", rotationX: 0, delay: 0, height: 125, ease: Circ.easeIn });
+    tl2.to(".letter", 1, { x: 0, y: 0, rotationX: 0, scaleY: 0.5, delay: 0, ease: Expo.easeOut });
+    tl2.to(".topfold", 0.6, { transformOrigin: "50% 0%", scaleY: 1, delay: 0, ease: Circ.easeIn })
+    flag = 3
+  }else if(flag==3){
+      alert("嗨呀，只能打开一次哦")
+  }
+
+
+}
+
+
+$("#email").submit(function (event) {
+  event.preventDefault();
+  var tl2 = new TimelineMax({
+    repeat: 0,
+    delay: 0
+  });
+
+  tl2.to(".upper", 1, { x: 0, transformOrigin: "50% 100%", rotationX: 0, delay: 0, height: 125, ease: Circ.easeIn });
+  tl2.to(".letter", 1, { x: 0, y: 0, rotationX: 0, scaleY: 0.5, delay: 0, ease: Expo.easeOut });
+  tl2.to(".topfold", 0.6, { transformOrigin: "50% 0%", scaleY: 1, delay: 0, ease: Circ.easeIn });
+  tl2.to(".wrapper", 0.7, { transformOrigin: "50% 0%", x: 5000, y: 0, delay: 0, ease: Circ.easeIn });
+
+
+});
